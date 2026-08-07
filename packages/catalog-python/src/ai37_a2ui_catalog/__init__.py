c2a7df6239ca99d949b7fcdd71c95a7757375695
cthtuf <gdazeysinc@gmail.com>
2026-08-07 12:08:22 +0300

    Add private repo

--- packages/catalog-python/src/ai37_a2ui_catalog/__init__.py
+++ packages/catalog-python/src/ai37_a2ui_catalog/__init__.py
@@ -1,8 +1,9 @@
-from .constants import CATALOG_ID, CATALOG_VERSION
+from .constants import A2UI_BASE_CATALOG_ID, CATALOG_ID, CATALOG_VERSION
 from .models import ChoiceCardProps, FlexTableProps, FormCardProps, LatexFormulaProps, SimpleTableProps
 from .validation import get_component_schema, validate_component_payload, validate_component_payload_json
 
 __all__ = [
+    "A2UI_BASE_CATALOG_ID",
     "CATALOG_ID",
     "CATALOG_VERSION",
     "ChoiceCardProps",
